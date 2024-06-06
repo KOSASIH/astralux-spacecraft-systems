6e431f5c758d15a246c69d4017ce243b58ce5adf
KOSASIH <kosasihg88@gmail.com>
2024-06-06 18:34:46 +0700

    Create data_transmission.py

---
+++ src/communication/data_transmission.py
@@ -0,0 +1,15 @@
+# Data transmission system for the spacecraft
+import socket
+
+class DataTransmission:
+    def __init__(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def connect(self, host, port):
+        self.socket.connect((host, port))
+
+    def send_data(self, data):
+        self.socket.sendall(data.encode())
+
+    def receive_data(self):
+        return self.socket.recv(1024).decode()
